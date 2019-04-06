7b2af8cdfc32c2a52112b5614bb75d2aca21075e
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-06 11:54:48 +0200

    Fix TypeScript defintion, refactor definition to CommonJS compatible export

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 'use strict';
 
-module.exports = (...iterable) => {
-	if (iterable.length === 0) {
+module.exports = (...functions) => {
+	if (functions.length === 0) {
 		throw new Error('Expected at least one argument');
 	}
 
-	return input => {
-		const iterator = iterable[Symbol.iterator]();
+	return async input => {
+		let currentValue = input;
 
-		const loop = async current => {
-			const {done, value} = iterator.next();
+		for (const fn of functions) {
+			currentValue = await fn(currentValue); // eslint-disable-line no-await-in-loop
+		}
 
-			if (done) {
-				return current;
-			}
-
-			const next = await value(current);
-			return loop(next);
-		};
-
-		return loop(input);
+		return currentValue;
 	};
 };
--- index.test-d.ts
+++ index.test-d.ts
@@ -1,62 +1,77 @@
-import {expectType} from 'tsd-check';
-import pipe from '.';
+import {expectType} from 'tsd';
+import pPipe = require('.');
 
-const unary = async (string: string) => `${string} Unicorn`;
-const binary = async (string: string, number: number) => unary(string.repeat(number));
-const ternary = async (string: string, number: number, boolean: boolean) => boolean ? binary(string, number) : unary(string);
+const fn = async (string: string) => `${string} Unicorn`;
 
 const identity = <T>(value: T) => value;
 const toNumber = (string: string) => parseInt(string);
 const toFixed = (number: number) => number.toFixed(2);
 
-// Unray
-expectType<string>(await pipe(unary)('❤️'));
-expectType<string>(await pipe(toNumber, toFixed)('❤️'));
+expectType<Promise<string>>(pPipe(fn)('❤️'));
+expectType<Promise<string>>(pPipe(toNumber, toFixed)('❤️'));
 
-expectType<string>(await pipe(unary, identity, identity)('❤️'));
-expectType<string>(await pipe(unary, identity, identity, identity)('❤️'));
-expectType<string>(await pipe(unary, identity, identity, identity, identity)('❤️'));
-expectType<string>(await pipe(unary, identity, identity, identity, identity, identity)('❤️'));
-expectType<string>(await pipe(unary, identity, identity, identity, identity, identity, identity)('❤️'));
-expectType<string>(await pipe(unary, identity, identity, identity, identity, identity, identity, identity)('❤️'));
-expectType<string>(await pipe(unary, identity, identity, identity, identity, identity, identity, identity, identity)('❤️'));
+expectType<Promise<string>>(pPipe(fn, identity, identity)('❤️'));
+expectType<Promise<string>>(pPipe(fn, identity, identity, identity)('❤️'));
+expectType<Promise<string>>(
+	pPipe(fn, identity, identity, identity, identity)('❤️')
+);
+expectType<Promise<string>>(
+	pPipe(fn, identity, identity, identity, identity, identity)('❤️')
+);
+expectType<Promise<string>>(
+	pPipe(fn, identity, identity, identity, identity, identity, identity)('❤️')
+);
+expectType<Promise<string>>(
+	pPipe(
+		fn,
+		identity,
+		identity,
+		identity,
+		identity,
+		identity,
+		identity,
+		identity
+	)('❤️')
+);
+expectType<Promise<string>>(
+	pPipe(
+		fn,
+		identity,
+		identity,
+		identity,
+		identity,
+		identity,
+		identity,
+		identity,
+		identity
+	)('❤️')
+);
 
-expectType<unknown>(await pipe(unary, identity, identity, identity, identity, identity, identity, identity, identity, identity)('❤️'));
-
-// Binary
-expectType<string>(await pipe(binary)('❤️', 3));
-expectType<number>(await pipe(binary, toNumber)('❤️', 10));
-
-expectType<string>(await pipe(binary, identity, identity)('❤️', 3));
-expectType<string>(await pipe(binary, identity, identity, identity)('❤️', 3));
-expectType<string>(await pipe(binary, identity, identity, identity, identity)('❤️', 3));
-expectType<string>(await pipe(binary, identity, identity, identity, identity, identity)('❤️', 3));
-expectType<string>(await pipe(binary, identity, identity, identity, identity, identity, identity)('❤️', 3));
-expectType<string>(await pipe(binary, identity, identity, identity, identity, identity, identity, identity)('❤️', 3));
-expectType<string>(await pipe(binary, identity, identity, identity, identity, identity, identity, identity, identity)('❤️', 3));
-
-expectType<unknown>(await pipe(binary, identity, identity, identity, identity, identity, identity, identity, identity, identity)('❤️', 12));
-
-// Tertary
-expectType<string>(await pipe(ternary)('❤️', 3, true));
-expectType<number>(await pipe(ternary, toNumber)('❤️', 10, false));
-
-expectType<string>(await pipe(ternary, identity, identity)('❤️', 3, true));
-expectType<string>(await pipe(ternary, identity, identity, identity)('❤️', 3, true));
-expectType<string>(await pipe(ternary, identity, identity, identity, identity)('❤️', 3, true));
-expectType<string>(await pipe(ternary, identity, identity, identity, identity, identity)('❤️', 3, true));
-expectType<string>(await pipe(ternary, identity, identity, identity, identity, identity, identity)('❤️', 3, true));
-expectType<string>(await pipe(ternary, identity, identity, identity, identity, identity, identity, identity)('❤️', 3, true));
-expectType<string>(await pipe(ternary, identity, identity, identity, identity, identity, identity, identity, identity)('❤️', 3, true));
-
-expectType<unknown>(await pipe(ternary, identity, identity, identity, identity, identity, identity, identity, identity, identity)('❤️', 12, false));
+expectType<unknown>(
+	pPipe(
+		fn,
+		identity,
+		identity,
+		identity,
+		identity,
+		identity,
+		identity,
+		identity,
+		identity,
+		identity
+	)('❤️')
+);
 
 // "Complex" examples
 const byPowerOfTwo = (number: number) => number ** 2;
 const asResult = async <T>(result: T) => ({result});
-const either = async (number: number) => number > 2 ? number : '🤪';
+const either = async (number: number) => (number > 2 ? number : '🤪');
 const count = (number: number) => [...Array(number).keys()];
 
-expectType<{result: string}>(await pipe(byPowerOfTwo, toFixed, asResult)(2));
-expectType<{result: number | string}>(await pipe(byPowerOfTwo, either, asResult)(2));
-expectType<number[]>(await pipe(byPowerOfTwo, count)(2));
+expectType<Promise<{result: string}>>(
+	pPipe(byPowerOfTwo, toFixed, asResult)(2)
+);
+expectType<Promise<{result: number | string}>>(
+	pPipe(byPowerOfTwo, either, asResult)(2)
+);
+expectType<Promise<number[]>>(pPipe(byPowerOfTwo, count)(2));
